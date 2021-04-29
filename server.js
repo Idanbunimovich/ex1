4d7c341972671049af8e2c7d539ef42e91c45042
Idanbunimovich <56599359+Idanbunimovich@users.noreply.github.com>
2021-04-29 03:43:36 +0300

    redis

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const moment = require('moment')
+const redis = require('redis')
+const {promisify} = require('util')
 
-
+let redisClient = redis.createClient({host:'demo-redis2.hh98yr.ng.0001.use1.cache.amazonaws.com',port:6379})
+let set = promisify(redisClient.set).bind(redisClient)
+let get = promisify(redisClient.get).bind(redisClient)
 
 
 const app = express();
@@ -11,17 +15,27 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(cors())
 app.use(bodyParser.json());
-let data = {
-    ticketId:0
-}
 
 
 app.post('/onBoard', async (req, res) =>{
-    data[`${data.ticketId}`] = req.query.plateId;
-    data[`${data.ticketId}`] = {};
-    data[`${data.ticketId}`].start = moment();
-    res.json(data.ticketId)
-    data.ticketId += 1;
+    let ticketId = await get('ticketId')
+    if(ticketId == null)
+    {
+        ticketId = 0
+    }
+    else{
+        ticketId = parseInt(ticketId)
+    }
+    res.json(ticketId)
+    let data = {
+        start:moment(),
+        plateId:req.query.plateId,
+        parkinLot:req.query.parkingLot
+    }
+    await set(ticketId,JSON.stringify(data))
+    ticketId += 1;
+    await set('ticketId',ticketId)
+
 
 
 });
@@ -30,10 +44,10 @@ app.post('/exit', async (req, res) =>{
 
     let {ticketId} = (req.query)
     let end = moment();
-
-    let diffHours = end.diff(data[`${ticketId}`].start, 'seconds');
+    let data = JSON.parse(await get(ticketId))
+    let diffHours = end.diff(data.start, 'seconds');
     let money = diffHours*10;
-    res.json(money)
+    res.json({platId: data.plateId,money,parkingLot:data.parkinLot})
 
 });
 app.get('/hi', async (req, res) => {
